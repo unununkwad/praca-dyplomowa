1718e943582d5d541e1daf90b498b5db85a684c6
unununkwad <69234051+unununkwad@users.noreply.github.com>
2021-01-21 14:59:13 +0100

    1. front połącznony z backendem
    21.01.2021

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="/css/main.css">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,900;1,500&display=swap" rel="stylesheet">
@@ -17,8 +17,8 @@
     
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rMWCnxwDxHyCapmgYTypH3f54XqkLYfxhZ8H+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSdAmvia1qwwkc2ssfu0sy7c6qhr8e4curh64j8vglc0pz0mqceunpvehrpfunpvehrpfunpvehrpfunpvehrpfunpvehrpfunpvehrpfunpvehrpfiGdIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
@@ -41,12 +41,26 @@
                         <li class="nav-item">
                             <a href="#" class="nav-link">Kontakt</a>
                         </li>
+                        @if (Route::has('login'))
+                <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
+                    @auth
                         <li class="nav-item">
-                            <a href="#" class="nav-link">Zaloguj</a>
+                            <a href="{{ url('/dashboard') }}" class="nav-link">dashboard</a>
                         </li>
+                    @else
                         <li class="nav-item">
-                            <a href="#" class="nav-link">Rejestracja</a>
+                            <a href="{{ route('login') }}" class="nav-link">Zaloguj</a>
                         </li>
+
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a href="{{ route('register') }}" class="nav-link">Rejestracja</a>
+                        </li>
+                        @endif
+                    @endif
+                </div>
+            @endif
+
                     </ul>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/home', function () {
+    return view('home');
+});
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
