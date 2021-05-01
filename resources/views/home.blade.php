8b1f3a466430545902b289f61e35def586f9f350
unununkwad <69234051+unununkwad@users.noreply.github.com>
2021-05-01 13:47:17 +0200

    poprawienie frontendu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
 
         
         @livewireStyles
+        @livewireScripts
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
@@ -33,164 +34,94 @@
 
 
     @livewire('navigation-dropdown')
-        <!--koniec paska nav bar-->
-        <!-- hero image -->
-        <section id="hero" class="d-flex align-items-center">
-            <div class="container text-center position-relative">
-                <h1>Wasze zdrowie to dla nas priorytet</h1>
-                <h2 class="text-uppercase">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, nam.</h2>
-                    @if (Route::has('login'))
-                    <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
-                        @auth
-                            <a href="/pacjent/termin" class="main-btn">Umów się na spotkanie</a>
-                        
-                    @else
-                        <a href="{{ route('login') }}" class="main-btn">Umów się na spotkanie</a>
-                        @endif
+    <!--koniec paska nav bar-->
+    <!-- hero image -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container text-center position-relative">
+            <h1>Wasze zdrowie to dla nas priorytet</h1>
+            <h2 class="text-uppercase">Robimy wszystko co w naszej mocy, aby nasi pacjenci byli zdrowi.</h2>
+                @if (Route::has('login'))
+                <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
+                    @auth
+                        <a href="/pacjent/termin" class="main-btn">Umów się na spotkanie</a>
+                    
+                @else
+                    <a href="{{ route('login') }}" class="main-btn">Umów się na spotkanie</a>
                     @endif
-            </div>
+                @endif
+        </div>
 
-        </section>
-        <!-- koniec hero image -->
-        <!-- usługi -->
-        <section id="services" class="services mt-5 py-3">
-            <div class="container">
-                <div class="section-title">
-                    <h2 class="text-center" id="uslugi">Usługi</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, excepturi natus. Facilis corporis sed harum!</p>
-                </div>
-                <div class="row">
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-tooth"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-lungs"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-heart"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-dna"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-ambulance"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
-                    <!--  -->
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-                        <div class="service shadow">
-                            <div class="icon">
-                                <i class="fas fa-allergies"></i>
-                            </div>
-                            <h4>Lorem ipsum</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus quod porro labore. Repellat, dolor aut nisi officiis perspiciatis hic?</p>
-                        </div>
-                    </div>
-                    <!--  -->
+    </section>
+    <!--  -->
+
+
+
+
+
+
+    <!-- O nas -->
+    <section class="about-section clearfix py-5" id="o_nas">
+        <div class="container">
+            <div class="about">
+                <img class="about-img img-fluid mb-3 mb-lg-0 rounded" src="../images/blur-hospital.jpg" alt="">
+                <div class="about-text left-0 text-center bg-faded p-5 rounded">
+                    <h2 class="about-heading mb-4">
+                        <span class="about-heading-upper">WITAMY</span>
+                        <span class="about-heading-lower">W przychodni ACME</span>
+                    </h2>
+                    <p class="mb-3"> Znajdziecie tutaj Państwo licznych specjalistów z zakresu podstawowej opieki zdrowotnej. Przychodnia ACME to miejsce, gdzie wszystkie starania koncentrują się wokół pacjenta i jego potrzeb. Szczególną uwagę zwróciliśmy na stworzenie przyjaznego otoczenia, w którym każdy poczuje się swobodnie i bezpiecznie. Jesteśmy przekonani, że połączenie wysokiej jakości oferowanych usług, dobrej organizacji pracy i obsługi pacjenta wraz z komfortowym wystrojem wnętrz sprawi, że miejsce to będzie gwarancją Twojego sukcesu na każdym etapie leczenia.
+                    </p>
                 </div>
             </div>
-        </section>
-        <!-- Koniec usług -->
+        </div>
+    </section>
+    <!--  -->
 
 
 
 
+    <!-- kontakt -->
+    <section class="contact" id="kontakt">
+        <div class="container">
+            <div class="section-title text-center mt-5">
+                <h2>Dane kontaktowe</h2><br>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact-box">
+                        <i class="fas fa-map-signs"></i>
+                        <h3>Adres</h3>
+                        <p>Pacanów 32</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="contact-box mt-4">
+                        <i class="fas fa-envelope"></i>
+                        <h3>Adres e-mail</h3>
+                        <p>adres1@email.pl<br>adres2@email.pl</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="contact-box mt-4">
+                        <i class="fas fa-phone"></i>
+                        <h3>Nr telefonu</h3>
+                        <p>+123 456 789 <br>+987 654 321</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--  -->
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        @livewireScripts
+    <!-- stopka -->
+    <footer class="footer py-4 mt-5">
+        <div class="container">
+                <div class="text">Copyright © Jakub Jastak 2021</div>
+        </div>
+    </footer>
+    <!--  -->
     </body>
 </html>
--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -7,13 +7,10 @@
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a href="/#uslugi" class="nav-link">Usługi</a>
+                    <a href="/#o_nas" class="nav-link">O nas</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">O nas</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Kontakt</a>
+                    <a href="/#kontakt" class="nav-link">Kontakt</a>
                 </li>
                 @if (Route::has('login'))
                 <!-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> -->
@@ -36,7 +33,7 @@
                                             <div>{{ Auth::user()->name }}</div>
 
                                             <div class="ml-1">
-                                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                                 </svg>
                                             </div>
